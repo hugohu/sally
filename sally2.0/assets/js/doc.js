3a6d3c11584ac2020a7cabad6a25624d02189a44
huugle <511194948@qq.com>
2014-04-08 17:35:04 +0800

    fix

--- sally2.0/assets/js/doc.js
+++ sally2.0/assets/js/doc.js
@@ -4,85 +4,139 @@
   @解析url 并且生成网页
    */
 var build = {
-  subNav: function() {
-    var nav = $(".nav");
-    $.getJSON("mod/nav.json", function(data) {
-      var html = "";
-      $.each(data.nav, function(i, n) {
-        var name = n.id;
-        var title = n.title;
-        html += "<a href=?p=" + name + ">" + title + "</a>";
-      });
-      nav.html(html);
-      $("a", nav).on("click", function(e) {
-        var $this = $(this);
-        build.setPage($this);
-        return false;
-      })
-    })
-  },
-  setSid: function(s) {
-    $.getJSON("mod/nav.json", function(data) {
-      var sid = $(".m-sidebar");
-      var html = '';
-      if (data[s] === undefined) {
-        $("body").addClass("f-page");
-        return false;
-      } else {
-        $("body").removeClass("f-page");
-      }
-      $.each(data[s], function(i, n) {
+  code: function(url) {
+    return $.ajax({
+      url: url,
+      async: false
+    }).responseText;
 
-        var name = n.id;
-        var title = n.title;
-        html += "<li><a href=#" + name + ">" + title + "</a></li>";
-      });
-      html = '<ul>' + html + '</ul>';
-      sid.html(html);
-    })
   },
-  setPage: function(e) {
-    var page = e.attr("href").slice(3);
-    this.setSid(page);
-    //生成主体内容
-    this.setMain(page);
-    return false;
+  addCss: function(mod, name) {
+    //可能有点问题
+    var cssfile = this.codedata.cssfile,
+      a = this.codedata[mod][name].style.split(";"),
+      b = [];
+    $.each(a, function(i, n) {
+      b[i] = cssfile + n + ".css"
+    })
+    var link = $("link");
+    var exp=$(".explain");
+    $.each(b, function(index, value) {
+      $("a").attr("href",value).html(value).appendTo(exp);
+      $("<link>").attr({
+        rel: "stylesheet",
+        type: "text/css",
+        href: value
+      }).appendTo("head");
+    });
+  },
+  params: function(s) {
+    // ?mod=modoles&name=m-box
+    var ret = {},
+      url = (s || location.search);
+    if (!url) {
+      ret["p"] = "index";
+      return ret;
+    }
+    var seg = url.replace(/^\?/, '').split('&'),
+      len = seg.length,
+      i = 0,
+      s;
+
+    for (; i < len; i++) {
+      if (!seg[i]) {
+        continue;
+      }
+      s = seg[i].split('=');
+      ret[s[0]] = s[1];
+    }
+    return ret;
+  },
+  html: function(mark, data, page) {
+    var setHtml = {
+      nav: function(data) {
+        var data = data.nav,
+          nav = $(".nav"),
+          html = "";
+        $.each(data, function(i, n) {
+          var name = n.id;
+          var title = n.title;
+          html += "<a href=\"?p=" + name + "\"data-page=" + name + ">" + title + "</a>";
+        });
+        nav.html(html);
+        $("a", nav).on("click", function(e) {
+          var $this = $(this),
+            page = $this.attr("data-page")
+            build.setpage(page, 0);
+          return false;
+        })
+      },
+      sidebar: function(data) {
+        var sid = $(".m-sidebar"),
+          html = '';
+        if (data != undefined) {
+          $("body").removeClass("f-page");
+        } else {
+          $("body").addClass("f-page");
+          return false;
+        }
+        $.each(data, function(i, n) {
+          var name = n.id;
+          var title = n.title;
+          html += "<li><a href=#" + name + ">" + title + "</a></li>";
+        });
+        html = '<ul>' + html + '</ul>';
+        sid.html(html);
+      },
+      main: function(data) {
+        var main = $("#main");
+        var html = build.mdToHtml(data);
+        main.html(html);
+        // Highlight syntax
+        $("code").each(function(i) {
+          hljs.highlightBlock(this);
+        });
+      }
+    }
+    setHtml.page = page;
+    //set page 
+    !! setHtml[mark] && setHtml[mark](data);
   },
   mdToHtml: function(md) {
     // require showdown.js 
     var converter = new Showdown.converter();
     return converter.makeHtml(md);
   },
-  setMain: function(s) {
-    var main = $(".m-main");
-    main.load("mod/html/" + s + ".md", function() {
-      var html = build.mdToHtml(main.html());
-      main.html(html);
+  setpage: function(p, mod) {
+    var code = "mod/code.json";
+    var o = this.params();
+    var p = p || o.p;
+    var mod = mod == 0 ? undefined : o.mod;
+    if (mod != undefined) {
+      var name = o.name;
+      if (this.codedata == undefined) {
+        var data = this.code(code);
+        this.codedata = $.parseJSON(data);
+      };
+      var htmlfile = this.codedata.htmlfile + mod + "/" + name + ".md";
+      this.addCss(mod, name)
+    } else {
+      var htmlfile = this.data.htmlfile + p + ".md";
 
-      // Highlight syntax
-      $("code").each(function(i) {
-        hljs.highlightBlock(this);
-      });
-    });
-  },
-  init: function() {
-    var page = location.search;
-    page = page != (undefined || "") ? page.slice(3) : "index";
-    //生成导航
-    this.subNav();
-    //生成侧边导航
-    this.setSid(page);
+    }
+    var smain = this.code(htmlfile);
+    this.html("sidebar", this.data[p]);
     //生成主体内容
-    this.setMain(page);
-
+    this.html("main", smain);
+  },
+  init: function(url) {
+    var data = this.code(url);
+    this.data = $.parseJSON(data);
+    //生成导航
+    this.html("nav", this.data);
+    this.setpage();
   }
 };
-build.init();
-
-$.getJSON("mod/code.json", function(data) {
-  console.log(data)
-  $.each(data.modoel, function(i, n) {
-    var name = n.id;
-    var title = n.title;
-  });
-})
+//start
+var url = "mod/nav.json";
+build.init(url);
--- sally2.0/assets/plugins/sallyedit/edit.js
+++ sally2.0/assets/plugins/sallyedit/edit.js
@@ -10,50 +10,85 @@ $(function() {
         clearTimeout(this.delay);
         this.delay = setTimeout(updatehtml, 300);
       });
+
       function updatehtml() {
         upe.innerHTML = editor.getValue();
       }
       setTimeout(updatehtml, 300);
       return editor;
     },
-    getText: function(url) {
+    code: function(url) {
       return $.ajax({
         url: url,
         async: false
       }).responseText;
+
     },
-    addCss: function(url) {
-      $("<link>")
-        .attr({
+    params: function(s) {
+      // ?mod=modoles&name=m-box
+      var ret = {},
+        url = (s || location.search);
+      if (!url) {
+        ret["p"] = "index";
+        return ret;
+      }
+      var seg = url.replace(/^\?/, '').split('&'),
+        len = seg.length,
+        i = 0,
+        s;
+
+      for (; i < len; i++) {
+        if (!seg[i]) {
+          continue;
+        }
+        s = seg[i].split('=');
+        ret[s[0]] = s[1];
+      }
+      return ret;
+    },
+    addCss: function(mod, name) {
+
+      //可能有点问题
+      var cssfile = this.codedata.cssfile;
+        if(this.codedata[mod][name]==undefined){
+          return false;
+        }
+      var  a = this.codedata[mod][name].style.split(";"),
+        b = [];
+      $.each(a, function(i, n) {
+        b[i] = cssfile + n + ".css"
+      })
+      var link = $("link");
+      var exp = $(".explain");
+      $.each(b, function(index, value) {
+        $("<a>").attr("href", value).html(value).appendTo(exp);
+        $("<link>").attr({
           rel: "stylesheet",
           type: "text/css",
-          href: url
-        })
-        .appendTo("head");
+          href: value
+        }).appendTo("head");
+      });
     },
-    getfile: function() {
-      var params = (function() {
-        var ret = {},
-          seg = location.search.replace(/^\?/, '').split('&'),
-          len = seg.length,
-          i = 0,
-          s;
-        for (; i < len; i++) {
-          if (!seg[i]) {
-            continue;
-          }
-          s = seg[i].split('=');
-          ret[s[0]] = s[1];
-        }
-        return ret;
-      })()
-      var cssfile = "mod/libs/",
-        htmlfile = "mod/html/code/";
-
-     return{
-      css: cssfile+params.name+".css",
-      html:htmlfile+params.name+".html",
-      style:params.style
+    setpage: function(p, mod) {
+      var code = "mod/code.json";
+      var o = this.params();
+      var p = p || o.p;
+      var mod = mod == 0 ? undefined : o.mod;
+      if (mod != undefined) {
+        var name = o.name;
+        if (this.codedata == undefined) {
+          var data = this.code(code);
+          this.codedata = $.parseJSON(data);
+        };
+        var htmlfile = this.codedata.htmlfile + mod + "/" + name + ".md";
+        var cssfile = this.codedata.cssfile + name + ".css"
+        this.addCss(mod, name)
+      } else {
+        return false;
+      }
+      return {
+        html: this.code(htmlfile),
+        css: this.code(cssfile)
       }
     }
   }
@@ -66,7 +101,11 @@ $(function() {
   var css = edit.add(css, 'text/css', editcss);
 
   //动态获取内容
-  css.setValue(name)
+  var setpage = edit.setpage();
+  var cssCode = setpage.css;
+  var htmlCode = setpage.html;
+  htmlCode && html.setValue(htmlCode)
+  cssCode && css.setValue(cssCode)
 });
 
 
@@ -82,6 +121,7 @@ $(".explain").on("mousedown", function(e) {
       $(".CodeMirror").css("height", _h + dix)
     }, 1)
   }
+
   function over(e) {
     $(document).off("mousemove", move);
     e.preventDefault();
